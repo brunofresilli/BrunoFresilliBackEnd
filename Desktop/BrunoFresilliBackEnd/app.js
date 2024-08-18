b0d7d75d3deedbcc3d5af156048df85ddc996db5
BrunoFresilli <BrunoFresilli11@gmail.com>
2024-08-18 16:56:15 -0300

    cambios

--- Desktop/BrunoFresilliBackEnd/app.js
+++ Desktop/BrunoFresilliBackEnd/app.js
@@ -81,8 +81,8 @@ app.set('views', path.join(__dirname, 'src', 'views'));
 // Middlewares
 
 app.use(addLogger); 
+app.use(express.urlencoded({ extended: true })); // Para procesar application/x-www-form-urlencoded
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 
